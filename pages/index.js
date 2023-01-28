06caef2b43f7da61404dfee08a46150b8e5813bc
lance-malaga <lmalaga@my.bcit.ca>
2023-01-28 15:28:18 -0800

    style home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import downArrow from '@/public/icons/downwardArrow.png'
+// components
+import NavBar from '@/components/NavBar'
+// images
+import down_arrow from '@/public/icons/downwardArrow.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,34 +19,39 @@ export default function Home() {
         <meta property="og:description"  content="BCIT Digital Design and Development Diploma" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* icon */}
-        <link rel="icon" href="../public/favicon.png" />
-        {/* fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
+        <link rel="icon" type="/image/png" href="/favicon.png" />
       </Head>
       <main className={styles.main}>
-        <h1>An investment in knowledge pays the best interest.</h1>
-        <p>
-          Different than a college or university, the British Columbia Institute of Technology offers practical, flexible, applied education with instructors who have direct, hands-on experience in their field.
-        </p>
-        <a>
-          <button>More About Us</button>
-        </a>
-        <a>
-          <button>Contact Us</button>
-        </a>
+        <NavBar/>
+        <div>
+          <div className={styles.main__content}>
+            <h1>An investment in knowledge pays the best interest.</h1>
+            <hr/>
+            <p>
+              Different than a college or university, the British Columbia Institute of Technology offers practical, flexible, applied education with instructors who have direct, hands-on experience in their field.
+            </p>
+            <div className={styles.main__content_buttons}>
+              <a>
+                <button>More About Us</button>
+              </a>
+              <a>
+                <button>Contact Us</button>
+              </a>
+            </div>
+          </div>
+          <a>
+            <span>
+              <Image
+                src={down_arrow}
+                alt={"down_arrow"}
+                className={styles.down_arrow}
+                height={40}
+                width={"auto"}
+              />
+            </span>
+          </a>
+        </div>
       </main>
-      <a>
-        <span>
-          <Image
-            src={downArrow}
-            alt={downArrow}
-            height={50}
-            width={"auto"}
-          />
-        </span>
-      </a>
     </>
   )
 }
