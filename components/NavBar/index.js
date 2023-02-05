d175300fddeca7a970cc0461af5e717d0f953a23
lance-malaga <lmalaga@my.bcit.ca>
2023-02-05 11:01:45 -0800

    develop menu state

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import styles from './styles.module.css'
 // assets
 import menu_icon from '@/public/icons/menu-icon.png'
 import grad_hat_icon from '@/public/icons/graduation-hat.png'
+import menu from '@/public/background-images/menuBackground.png'
+import Link from 'next/link'
 
 export default function NavBar () {
+
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <>
             <nav className={styles.navbar}>
@@ -13,6 +19,7 @@ export default function NavBar () {
                     alt={"menu_icon"}
                     height={35}
                     width={"auto"}
+                    onClick={() => setShowMenu(true)}
                 />
                 <Image
                     src={grad_hat_icon}
@@ -21,6 +28,30 @@ export default function NavBar () {
                     width={"auto"}
                 />
             </nav>
+            {showMenu && (
+                <div className={styles.menu}>
+                    <Image
+                        src={menu}
+                        alt={'menu-background'}
+                        width={"auto"}
+                        height={300}
+                    />
+                    <div>
+                        <h1 onClick={() => setShowMenu(false)}>X</h1>
+                        <ul>
+                            <li>
+                                <Link href='/'> Home </Link>
+                            </li>
+                            <li>
+                                <Link href='/about'> About </Link>
+                            </li>
+                            <li>
+                                <Link href='/contact'> Contact </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
