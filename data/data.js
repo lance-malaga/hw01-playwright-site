7285aca336481fc059feec08e54200997a653129
lance-malaga <lmalaga@my.bcit.ca>
2023-02-05 11:02:08 -0800

    change data to js file

---
+++ data/data.js
@@ -0,0 +1,27 @@
+export const carousel = [
+    {
+        "id": 0, 
+        "value": "Achieve Excellence",
+        "image": "/carousel-images/0.jpg"
+    },
+    {
+        "id": 1, 
+        "value": "Embrace Innovation",
+        "image": "/carousel-images/1.jpg"
+    },
+    {
+        "id": 2, 
+        "value": "Champion Diversity",
+        "image": "/carousel-images/2.jpg"
+    },
+    {
+        "id": 3, 
+        "value": "Pursue Collaboration",
+        "image": "/carousel-images/3.jpg"
+    },
+    {
+        "id": 4, 
+        "value": "Engage With Respect",
+        "image": "/carousel-images/4.jpg"
+    }
+];
