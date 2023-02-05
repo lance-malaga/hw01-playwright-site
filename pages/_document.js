ef8a9590790ee6a70c3dd7ef7c9ed7c3e7851084
lance-malaga <lmalaga@my.bcit.ca>
2023-02-05 11:02:29 -0800

    change to crossOrigin

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         {/* fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
