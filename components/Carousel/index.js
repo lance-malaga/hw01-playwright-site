0254ab56a9b327348e64c4a6e644556c042a17b4
lance-malaga <lmalaga@my.bcit.ca>
2023-02-05 10:59:55 -0800

    develop carousel component

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -1,27 +1,64 @@
 import Image from 'next/image'
 import styles from './styles.module.css'
-// assets
-import left_arrow from '@/public/icons/leftArrow.png'
-import right_arrow from '@/public/icons/rightArrow.png'
+import { useState } from 'react'
+import { carousel } from '@/data/data'
 
-export default function Carousel({
-    
-}) {
+export default function Carousel() {
+
+    const [carouselData, setCarouselData] = useState([...carousel]);
+    const [imgIndex, setImgIndex] = useState(0);
+
+    const nextSlide = () => {
+        setImgIndex(imgIndex + 1);
+        if (imgIndex == carouselData.length - 1) {
+            setImgIndex(0);
+        }
+    }
+    const prevSlide = () => {
+        setImgIndex(imgIndex - 1);
+        if (imgIndex == 0) {
+            setImgIndex(carouselData.length-1);
+        }
+        console.log(imgIndex + "inside function")
+    }
+
     return (
-        <div className={styles.carousel}>
-
+        <div 
+            className={styles.carousel} 
+            id="carouselImage"
+            style={{ height: "200px", backgroundRepeat: "no-repeat", backgroundSize: "cover", display: "flex", justifyContent: "center", alignItems: "center", margin: "40px 0px" }}
+        >
             <Image
-                src={left_arrow}
-                alt={"left_arrow"}
-                height={30}
-                width={"auto"}
+                className={styles.carouselImg}
+                src={carouselData[imgIndex].image}
+                key={carouselData[imgIndex].id}
+                alt={carouselData[imgIndex].value}
+                height={200}
+                width={400}
+                priority
             />
-            <Image
-                src={right_arrow}
-                alt={"right_arrow"}
-                height={30}
-                width={"auto"}
-            />
+            <div>
+                <Image
+                    src="/icons/leftArrow.png"
+                    alt={"left_arrow"}
+                    height={30}
+                    width={40}
+                    onClick={prevSlide}
+                />
+                <ul 
+                    id="testOnImageHere"
+                    style={{ backgroundColor: "rgba(16, 46, 53, 0.5)", padding: "10px", margin: "0px 50px", listStyle: "none", textAlign: "center" }}
+                >
+                    <li>{carouselData[imgIndex].value}</li>
+                </ul>
+                <Image
+                    src="/icons/rightArrow.png"
+                    alt={"right_arrow"}
+                    height={30}
+                    width={40}
+                    onClick={nextSlide}
+                />
+            </div>
         </div>
     )
 }
